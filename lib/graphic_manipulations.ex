2e7a784b0642d34b6890f3ec8070c65d970381a1
Brandon Tindle <bjtactor@me.com>
2018-09-04 19:01:52 -0400

    Updated gitignore

--- lib/graphic_manipulations.ex
+++ lib/graphic_manipulations.ex
@@ -16,25 +16,40 @@ defmodule GraphicManipulations do
   # 3. Will save to root
 
   ## Examples
-      iex> GraphicManipulations.resize("bitstrings.png", 200, 113)
-      iex> GraphicManipulations.resize("bitstrings_1.png", 2000, 1000, [in_place: true])
-      iex> GraphicManipulations.resize("bitstrings.png", 200, 113, [path: "bitstrings_resized.png"])
+      iex> GraphicManipulations.resize("bitstrings.png", "gif", 200, 113)
+      iex> GraphicManipulations.resize("bitstrings_1.png", "gif", 2000, 1000, [in_place: true])
+      iex> GraphicManipulations.resize("bitstrings.png", "gif", 200, 113, [path: "bitstrings_resized.gif"])
       %Mogrify.Image{
         animated: false,
         dirty: %{},
-        ext: ".png",
-        format: nil,
+        ext: ".gif",
+        format: "gif",
         frame_count: 1,
         height: nil,
         operations: [],
-        path: "bitstrings_resized.png",
+        path: "bitstrings_resized.gif",
         width: nil
       }
+      iex> GraphicManipulations.resize("bitstrings.png", "gifx", 200, 113, [path: "bitstrings_resized.gifx"])
+      {:error, "Supplied format gifx not supported"}
 
   """
-  def resize(path, width, height, opts \\ []) do
+  def resize(path, format, width, height, opts \\ [])
+
+  def resize(path, format, width, height, opts) when format == "png" do
     open(path)
     |> resize_to_limit(~s(#{width}x#{height}))
     |> save(opts)
+  end
+
+  def resize(path, format, width, height, opts) when format == "gif" do
+    open(path)
+    |> resize_to_limit(~s(#{width}x#{height}))
+    |> format("gif")
+    |> save(opts)
+  end
+
+  def resize(_path, format, _width, _height, _opts) do
+    {:error, "Supplied format #{format} not supported"}
   end
 end
